819d3c992a7b5da52889f06281ee65d00df940d5
Jasmin Foerstel <jasminf1508@gmail.com>
2025-06-10 18:37:34 +0200

    #13 Implement Type Checking for Assignments

--- src/main/java/de/dhbw/mh/rinne/ast/AstAssignmentNode.java
+++ src/main/java/de/dhbw/mh/rinne/ast/AstAssignmentNode.java
@@ -1,10 +1,11 @@
 package de.dhbw.mh.rinne.ast;
 
 import de.dhbw.mh.rinne.CodeLocation;
+import de.dhbw.mh.rinne.RinneType;
 
 public class AstAssignmentNode extends AstStmtNode {
     private final String name;
-    private final AstExpressionNode value;
+    private AstExpressionNode value;
 
     public AstAssignmentNode(CodeLocation codeLocation, String name, AstExpressionNode value) {
         super(codeLocation);
@@ -16,5 +17,17 @@ public class AstAssignmentNode extends AstStmtNode {
     @Override
     public <T> T accept(AstVisitor<T> visitor) {
         return visitor.visitAssignment(this);
+    }
+
+    public AstExpressionNode value() {
+        return value;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void castValue(RinneType targetType) {
+        value = new AstCastNode(value, targetType);
     }
 }
